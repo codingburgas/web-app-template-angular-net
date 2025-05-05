7f910c022da1e0c35d6f9d6f4e0e26103cc3ad2d
kddinev18 <kddinev18@codingburgas.bg>
2025-05-05 12:36:33 +0300

    Fix dependency issues

--- WEB/src/app/app.component.spec.ts
+++ WEB/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'job-traking' title`, () => {
+  it(`should have the 'job-training' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('job-traking');
+    expect(app.title).toEqual('job-training');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, job-traking');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, job-training');
   });
 });
