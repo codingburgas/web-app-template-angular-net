5eb8548490b4142426bebd7f930ccd49521c7192
kddinev18 <kddinev18@codingburgas.bg>
2025-05-05 12:55:28 +0300

    Configure Angular project

--- WEB/src/app/app.component.ts
+++ WEB/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [CommonModule, RouterOutlet, MatSlideToggleModule, TranslateModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    standalone: true,
 })
 export class AppComponent {
   title = 'job-training';
