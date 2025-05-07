c65c55516f10c547b43cc9995906c74f96dd2b6f
idiliev18 <idiliev2004@gmail.com>
2025-05-07 10:05:01 +0300

    Replace Material/Tailwind with Bootstrap

--- WEB/src/app/app.component.ts
+++ WEB/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-root',
-    imports: [CommonModule, RouterOutlet, MatSlideToggleModule, TranslateModule],
+    imports: [CommonModule, RouterOutlet, TranslateModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     standalone: true,
